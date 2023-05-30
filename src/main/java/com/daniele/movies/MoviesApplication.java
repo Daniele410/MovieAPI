883975ab1e295c3a50adf6ed0baea6519644cb44
Daniele410 <miraglia.daniele410@gmail.com>
2023-05-30 13:30:06 +0200

    first commit develop

--- src/main/java/com/daniele/movies/MoviesApplication.java
+++ src/main/java/com/daniele/movies/MoviesApplication.java
@@ -4,16 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@RestController
 public class MoviesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MoviesApplication.class, args);
     }
-
-//	@GetMapping("/root")
-//	public String apiRoot(){
-//		return "Hello, World!";
-//	}
 
 }
